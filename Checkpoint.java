7fdd9b7088b0592bb0bb510d80256b05688ced70
Samir Daraji <302575838@student.rocmondriaan.nl>
2018-11-15 13:58:06 +0100

    Made checkpoint flag

---
+++ Checkpoint.java
@@ -0,0 +1,20 @@
+
+/**
+ *
+ * Write a description of class Checkpoint here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Checkpoint extends Mover {    
+    public Checkpoint() {
+        setImage("flagBlueHanging.png");
+}
+
+    public void act(){
+        applyVelocity();
+    }
+    
+  
+}
+
