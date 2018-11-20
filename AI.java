d6459771a08a7474158194c98b706e81b2c6f929
Samir Daraji <302575838@student.rocmondriaan.nl>
2018-11-20 09:34:44 +0100

    new ai

---
+++ AI.java
@@ -0,0 +1,15 @@
+import greenfoot.*;
+public class AI  extends Mover {
+    
+    private int x;
+    private int y;
+
+    public AI()
+    {
+    }
+
+    public int sampleMethod(int y)
+    {
+        return x + y;
+    }
+}
