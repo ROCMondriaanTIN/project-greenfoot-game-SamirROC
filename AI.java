ee9530658e4a8eb014a666e14a1825d9703c584f
Samir Daraji <302575838@student.rocmondriaan.nl>
2018-11-21 09:29:17 +0100

    Added animation and velocity to fly AI

--- AI.java
+++ AI.java
@@ -1,7 +1,7 @@
 import greenfoot.*;
 public class AI  extends Mover {
     
-    public GreenfootImage fly1f = new GreenfootImage("flyFly1+.png");
+    public GreenfootImage fly1f = new GreenfootImage("flyFly1.png");
     public GreenfootImage fly2f = new GreenfootImage("flyFly2.png");
     private int x;
     private int y;
@@ -9,9 +9,24 @@ public class AI  extends Mover {
     public AI()
     {
         setImage(fly1f);
+        velocityY = -1;
+        velocityX = 0;
     }
+    
+    public void flyMove() {
+    if (getImage().equals(fly1f)) 
+     {  
+       setImage (fly2f);  
+     }
+     else 
+     {  
+       setImage (fly1f);   
+     }
+}
     
    public void act() {
         applyVelocity();  
+        flyMove();
     }
 }
+
