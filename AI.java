164c2d49ba59073bcbb33015d356327b1751fd9d
Samir Daraji <302575838@student.rocmondriaan.nl>
2018-11-22 10:13:13 +0100

    added walkrange, xmin, xmax, speed, firstact

--- AI.java
+++ AI.java
@@ -5,16 +5,23 @@ public class AI  extends Mover {
     public GreenfootImage fly2f = new GreenfootImage("flyFly2.png");
     private int x;
     private int y;
+    private int xMin = 500;
+    private int xMax = 500;
+    private int speed;
+    private int walkRange;
+    private boolean firstAct;
     
     public AI()
     {
         setImage(fly1f);
-        velocityY = -1;
-        velocityX = 0;
+        velocityY = -3;
+        walkRange = 200;
+        firstAct = true;
+        speed = 5;
     }
     
     public void flyMove() {
-    if (getImage().equals(fly1f)) 
+   if (getImage().equals(fly1f)) 
      {  
        setImage (fly2f);  
      }
@@ -25,8 +32,25 @@ public class AI  extends Mover {
 }
     
    public void act() {
-        applyVelocity();  
+        velocityX = speed;
+        applyVelocity();
+        int x = getX();
+        int y = getY();
+
+        if (firstAct) {
+            firstAct = false;
+            xMin = x - walkRange / 2;
+            xMax = x + walkRange / 2;
+        }
+        if (getX() >= xMax) {
+            speed *= -1;
+            x = xMax;
+        } else if (getX() <= xMin) {
+            speed *= -1;
+            x = xMin;
+        }
         flyMove();
     }
 }
+
 
