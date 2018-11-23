f3978d175cabf6cd845d3f345c05d0d42d7205da
Samir Daraji <302575838@student.rocmondriaan.nl>
2018-11-23 09:37:55 +0100

    added hud overlay

---
+++ Overlay.java
@@ -0,0 +1,12 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+public class Overlay extends Actor
+{
+    
+    public Overlay(){
+        getWorld().addObject( new Lifes(),960, 1550);
+    }
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
