a2840585a80f84bb8b1245967ea48e3657f768bf
Samir Daraji <302575838@student.rocmondriaan.nl>
2018-12-03 11:56:03 +0100

    save

--- MyWorld.java
+++ MyWorld.java
@@ -244,7 +244,7 @@ public class MyWorld extends World {
 
         // Alle objecten toevoegen aan de wereld: camera, main karakter en mogelijke enemies
         addObject(camera, 0, 0);
-        addObject(hero, 960, 10000); //960, 11650);
+        addObject(hero, 960, 11650);
         addObject(new Enemy2(), 452, 11555);
         addObject(new Enemy(), 505, 10187);
         addObject(new Enemy(), 745, 10187);
